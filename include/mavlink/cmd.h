032e7a731c94cabc9f31bdf022085f1149c1f60c
Brian Taylor <brian.taylor@bolderflight.com>
2021-04-10 08:11:47 -0600

    Roughing in cmd microservice

--- include/mavlink/cmd.h
+++ include/mavlink/cmd.h
@@ -22,3 +22,71 @@
 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 * IN THE SOFTWARE.
 */
+
+#ifndef INCLUDE_MAVLINK_CMD_H_
+#define INCLUDE_MAVLINK_CMD_H_
+
+#include "core/core.h"
+#include "mavlink_types.h"
+#include "common/mavlink.h"
+#include "mavlink/mission.h"
+
+namespace bfs {
+
+struct CmdItem {
+  MissionCmd cmd;
+  double param[7];
+};
+
+class MavLinkCmd {
+ public:
+  MavLinkCmd(HardwareSerial *bus) : bus_(bus) {}
+  MavLinkCmd(HardwareSerial *bus, const uint8_t sys_id) : bus_(bus),
+                                                          sys_id_(sys_id) {}
+  /* Getters */
+  inline constexpr uint8_t sys_id() const {return sys_id_;}
+  inline constexpr uint8_t comp_id() const {return comp_id_;}
+  /* Get cmd */
+  inline bool new_cmd() {
+    bool status = new_cmd_;
+    new_cmd_ = false;
+    return status;
+  }
+  inline bool cancel_cmd() const {return cancel_cmd_;}
+  inline CmdItem cmd() {return cmd_item_;}
+  /* Cmd status */
+  void CmdComplete();
+  void CmdInProgress(int8_t progress);
+  void CmdInvalid();
+  void CmdUnsupported();
+  void CmdCancelled();
+
+  void MsgHandler(const mavlink_message_t &ref);
+ private:
+  /* Serial bus */
+  HardwareSerial *bus_;
+  /* Config */
+  const uint8_t sys_id_ = 1;
+  static const uint8_t comp_id_ = MAV_COMP_ID_AUTOPILOT1;
+  /* Message buffer */
+  mavlink_message_t msg_;
+  uint16_t msg_len_;
+  uint8_t msg_buf_[MAVLINK_MAX_PACKET_LEN];
+  /* State */
+  bool new_cmd_ = false;
+  bool cancel_cmd_ = false;
+  int cmd_ = -1;
+  CmdItem cmd_item_;
+  /* Message handlers */
+  uint8_t rx_sys_id_, rx_comp_id_;
+  void CmdIntHandler(const mavlink_command_int_t & ref);
+  void CmdLongHandler(const mavlink_command_long_t &ref);
+  void CmdCancelHandler(const mavlink_command_cancel_t &ref);
+  /* Message senders */
+  void SendCmdAck(const uint8_t result, const uint8_t progress);
+
+};
+
+}  // namespace bfs
+
+#endif  // INCLUDE_MAVLINK_CMD_H_
--- src/mavlink/cmd.cc
+++ src/mavlink/cmd.cc
@@ -23,7 +23,108 @@
 * IN THE SOFTWARE.
 */
 
-#include "mavlink/heartbeat.h"
+#include <array>
+#include "mavlink/cmd.h"
 #include "core/core.h"
 #include "mavlink_types.h"
 #include "common/mavlink.h"
+
+namespace bfs {
+
+void MavLinkCmd::MsgHandler(const mavlink_message_t &ref) {
+  rx_sys_id_ = ref.sysid; 
+  rx_comp_id_ = ref.compid;
+  switch (ref.msgid) {
+    case MAVLINK_MSG_ID_COMMAND_INT: {
+      mavlink_command_int_t cmd_int;
+      mavlink_msg_command_int_decode(&ref, &cmd_int);
+      CmdIntHandler(cmd_int);
+      break;
+    }
+    case MAVLINK_MSG_ID_COMMAND_LONG: {
+      mavlink_command_long_t cmd_long;
+      mavlink_msg_command_long_decode(&ref, &cmd_long);
+      CmdLongHandler(cmd_long);
+      break;
+    }
+    case MAVLINK_MSG_ID_COMMAND_CANCEL: {
+      mavlink_command_cancel_t cmd_cancel;
+      mavlink_msg_command_cancel_decode(&ref, &cmd_cancel);
+      CmdCancelHandler(cmd_cancel);
+      break;
+    }
+  }
+}
+void MavLinkCmd::CmdIntHandler(const mavlink_command_int_t & ref) {
+  if ((ref.target_system == sys_id_) && (ref.target_component == comp_id_)) {
+    if (cmd_ == -1) {
+      new_cmd_ = true;
+      cmd_ = ref.command;
+      cancel_cmd_ = false;
+      cmd_item_.param[0] = static_cast<double>(ref.param1);
+      cmd_item_.param[1] = static_cast<double>(ref.param2);
+      cmd_item_.param[2] = static_cast<double>(ref.param3);
+      cmd_item_.param[3] = static_cast<double>(ref.param4);
+      cmd_item_.param[4] = static_cast<double>(ref.x);
+      cmd_item_.param[5] = static_cast<double>(ref.y);
+      cmd_item_.param[6] = static_cast<double>(ref.z);
+    } else {
+      SendCmdAck(MAV_RESULT_TEMPORARILY_REJECTED, 255);
+    }
+  }
+}
+void MavLinkCmd::CmdLongHandler(const mavlink_command_long_t &ref) {
+  if ((ref.target_system == sys_id_) && (ref.target_component == comp_id_)) {
+    if (cmd_ == -1) {
+      new_cmd_ = true;
+      cmd_ = ref.command;
+      cancel_cmd_ = false;
+      cmd_item_.param[0] = static_cast<double>(ref.param1);
+      cmd_item_.param[1] = static_cast<double>(ref.param2);
+      cmd_item_.param[2] = static_cast<double>(ref.param3);
+      cmd_item_.param[3] = static_cast<double>(ref.param4);
+      cmd_item_.param[4] = static_cast<double>(ref.param5);
+      cmd_item_.param[5] = static_cast<double>(ref.param6);
+      cmd_item_.param[6] = static_cast<double>(ref.param7);
+    } else {
+      SendCmdAck(MAV_RESULT_TEMPORARILY_REJECTED, 255);
+    }
+  }
+}
+void MavLinkCmd::CmdCancelHandler(const mavlink_command_cancel_t &ref) {
+  if ((ref.target_system == sys_id_) && (ref.target_component == comp_id_)) {
+    if (ref.command == cmd_) {
+      cancel_cmd_ = true;
+    }
+  }
+}
+void MavLinkCmd::CmdComplete() {
+  SendCmdAck(MAV_RESULT_ACCEPTED, 255);
+  cmd_ = -1;
+}
+void MavLinkCmd::CmdInProgress(int8_t progress) {
+  if ((progress >= 0) && (progress <= 100)) {
+    SendCmdAck(MAV_RESULT_IN_PROGRESS, static_cast<uint8_t>(progress));
+  } else {
+    SendCmdAck(MAV_RESULT_IN_PROGRESS, 255);
+  }
+}
+void MavLinkCmd::CmdInvalid() {
+  SendCmdAck(MAV_RESULT_DENIED, 255);
+  cmd_ = -1;
+}
+void MavLinkCmd::CmdUnsupported() {
+  SendCmdAck(MAV_RESULT_UNSUPPORTED, 255);
+  cmd_ = -1;
+}
+void MavLinkCmd::CmdCancelled() {
+  SendCmdAck(MAV_RESULT_CANCELLED, 255);
+  cmd_ = -1;
+}
+void MavLinkCmd::SendCmdAck(const uint8_t result, const uint8_t progress) {
+  int32_t result_param2 = 0;
+  msg_len_ = mavlink_msg_command_ack_pack(sys_id_, comp_id_, &msg_,
+      cmd_, result, progress, result_param2, rx_sys_id_, rx_comp_id_);
+}
+
+}  // namespace bfs
